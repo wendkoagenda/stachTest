1267b1c8ddd1664d431d2484cdea69a45a08f270
wendkoagenda <elisee.wou@gmail.com>
2024-02-10 00:09:44 +0000

    Debut de la barre de navigation

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -1,3 +1,46 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { Card } from "../ui/card";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className="flex justify-between border border-cyan-600 bg-transparent filter">
+        <div>
+          <div className="flex gap-3">
+            Logo
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  {" "}
+                  <Link to="/" className="font-medium">
+                    Home
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Page d'accueil</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
+        </div>
+        <div>Profile</div>
+      </div>
+    </>
+  );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,92 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Footer from "../../components/commons/Footer";
 import Header from "../../components/commons/Header";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
     <>
       <Header />
-      <div className="w-full mx-auto p-9 border border-gray-300">
+      <div className="w-full mx-auto p-9 border border-gray-300 bg-slate-950">
         <div className="grid lg:grid-cols-4  md:grid-cols-2 gap-4">
-          <div className="border border-red-600 ">1</div>
+          <div className="border border-red-600 ">
+            <Button>Click me</Button>
+          </div>
           <div className="border border-red-600 ">2</div>
           <div className="border border-red-600 ">3</div>
           <div className="border border-red-600 ">4</div>
+        </div>
+        <div className="flex flex-row justify-center">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>
+                Ceci est la description du login
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-8"
+                >
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                          <Input placeholder="username" {...field} />
+                        </FormControl>
+                        <FormDescription>
+                          This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit">Submit</Button>
+                </form>
+              </Form>
+            </CardContent>
+            <CardFooter>Je suis le footer</CardFooter>
+          </Card>
         </div>
       </div>
       <Footer />
