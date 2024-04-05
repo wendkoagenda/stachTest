5437cfb2fec5071032e0885da69e3c46310749da
wendkoagenda <elisee.wou@gmail.com>
2024-04-05 06:43:11 +0000

    Mise ajour du footer

--- src/components/partials/Footer.tsx
+++ src/components/partials/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-white p-3  backdrop-blur-lg filter flex justify-between">
+    <div className="fixed bottom-0 left-0 w-full  p-3  backdrop-blur-lg filter flex justify-between">
       <p className="text text-xs"> © 2024 CampusFlow. Tous droits réservés.</p>
     </div>
   );
